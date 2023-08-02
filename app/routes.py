ea12990fc1a4ed2cd7dc61723771f8344e6b71f5
Davidpeca <141173655+Davidpeca@users.noreply.github.com>
2023-08-02 17:25:05 -0300

    novas etapas

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template
+from app.forms import Contato
 
 @app.route('/')
 def index():
@@ -8,3 +9,16 @@ def index():
 @app.route('/sobre')
 def sobre():
     return render_template("sobre.html",title="sobre mim")
+
+@app.route('/contato')
+def contato():
+    return render_template("contato.html",title="contato")
+
+@app.route('/projeto')
+def projeto():
+    return render_template("projeto.html",title="projeto")
+
+@app.route('/enviar-contato')
+def enviar():
+    formulario= Contato()
+    return render_template("enviar.html",title="projeto")
